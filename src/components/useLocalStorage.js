cada8f873736393c011ddc074391b3c1398e537f
Shaikh Irfan <shaikhji1998@gmail.com>
2022-12-20 23:39:18 +0530

    new Commit

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -2,7 +2,14 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const useLocalStorage = (stateVar, value) => {
-  
+    const [presentState, setState] = useState(value);
+    updateStorage(stateVar, presentState);
+    return [presentState, setState];
+
 };
+
+const updateStorage = (key, value) => {
+    localStorage.setItem(key, value);
+}
 
 export default useLocalStorage;
